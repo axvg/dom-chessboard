917b72d13faa9aae0db2ef33b7638e7c2c499e5b
axvg <91773368+axvg@users.noreply.github.com>
2023-02-25 17:38:07 -0500

    Add Map to simplify highlight function

--- js/chessboard.js
+++ js/chessboard.js
@@ -3,70 +3,51 @@ export default {
   highlight
 };
 
-var tiles = [];
+var diagonals = []
+var tileDiagonals = new Map()
+var highlighted = []
 
 // ****************************
 
 function draw(boardEl) {
+  for (let i = 0; i < 30; i++) {
+    diagonals.push([])
+  }
+
+
   for (let i = 0; i < 8; i++) {
     let rowEl = document.createElement("div")
-    let rowTiles = []
-    tiles.push(rowTiles)
     for (let j = 0; j < 8; j++) {
       let tileEl = document.createElement("div");
-      tileEl.dataset.row = i
-      tileEl.dataset.col = j
+
+      let majorDiagIdx = diagonals[7 - (i - j)]
+      let minorDiagIdx = diagonals[15 + (i + j)]
+
+      majorDiagIdx.push(tileEl);
+      minorDiagIdx.push(tileEl);
+      tileDiagonals.set(tileEl, [majorDiagIdx, minorDiagIdx])
+
       rowEl.appendChild(tileEl)
-      rowTiles.push(tileEl)
     }
     boardEl.appendChild(rowEl)
   }
 }
 
 function highlight(tileEl) {
-  // TODO: clear previous highlights (if any) and
-  // then find the tiles in the two diagonals
-  // (major and minor) that `tileEl` belongs to,
-  // to highlight them via CSS class "highlighted"
-
   // clear all currently highlighted tiles
-  for (let row of tiles) {
-    for (let el of row) {
+  for (let diagonal of highlighted) {
+    for (let el of diagonal) {
       el.classList.remove("highlighted")
     }
   }
 
   if (tileEl) {
-    let rowEl = tileEl.parentNode
-    let boardEl = rowEl.parentNode
+    highlighted = tileDiagonals.get(tileEl)
 
-    let tileRowIdx = Number(tileEl.dataset.row)
-    let tileColIdx = Number(tileEl.dataset.col)
-
-    // highlight in the up-left direction
-    for (let i = tileRowIdx, j = tileColIdx; i >= 0 && j >= 0; i--, j--) {
-      let el = findTile(i, j)
-      el.classList.add("highlighted")
-    }
-    // highlight in the up-right direction
-    for (let i = tileRowIdx, j = tileColIdx; i >= 0 && j < 8; i--, j++) {
-      let el = findTile(i, j)
-      el.classList.add("highlighted")
-    }
-    // highlight in the down-left direction
-    for (let i = tileRowIdx, j = tileColIdx; i < 8 && j >= 0; i++, j--) {
-      let el = findTile(i, j)
-      el.classList.add("highlighted")
-    }
-    // highlight in the down-right direction
-    for (let i = tileRowIdx, j = tileColIdx; i < 8 && j < 8; i++, j++) {
-      let el = findTile(i, j)
-      el.classList.add("highlighted")
+    for (let diagonal of highlighted) {
+      for (let el of diagonal) {
+        el.classList.add("highlighted")
+      }
     }
   }
-}
-
-function findTile(row, col) {
-  // return document.querySelector(`#board > div:nth-child(${row + 1}) > div:nth-child(${col + 1})`)
-  return tiles[row][col]
 }
